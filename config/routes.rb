fed5aa070a39c82723719d6c73756f4ec8569496
Alizee Vallat <alizee.v@live.fr>
2023-03-25 22:00:23 +0100

    bookmark controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "lists#index"
 
-  resources :lists, only: %i[index show new create]
-
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: :delete
 end

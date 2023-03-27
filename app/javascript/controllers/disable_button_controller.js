8b95525f4f56f7119ad1b7bc20eaa4ee8e25222c
Alizee Vallat <alizee.v@live.fr>
2023-03-27 16:36:52 +0200

    stimulus and typed js

---
+++ app/javascript/controllers/disable_button_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="disable-button"
+export default class extends Controller {
+  connect() {
+    console.log("Hello from our first Stimulus controller")
+  }
+
+  // disable() {
+  //   this.element.setAttribute("disabled", "")
+  // }
+}
